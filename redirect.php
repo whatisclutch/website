38aa490accd3b4c1c778ea2850f4be72c9a13f62
Victor <zinomaker@hotmail.com>
2012-08-26 14:14:43 +1000

    website files

---
+++ redirect.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+&lt;?php <br>
+header("HTTP/1.1 301 Moved Permanently"); <br>
+header("Location: http://www.whatisclutch.com/wic_uc.html"); <br>
+exit(); <br>
+?&gt;
+</body>
+</html>
